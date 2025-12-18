4da2a445ef747649a15e2af298b12db12d775e7a
adisaomoabegunde <oluwatobilobaadedapo81@gmail.com>
2025-12-18 13:12:09 +0100

    Fixxx

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
-import child_process from 'child_process';
+// import fs from 'fs';
+// import path from 'path';
+// import child_process from 'child_process';
 import { env } from 'process';
 
 // import { defineConfig } from 'vite'
@@ -14,35 +14,35 @@ import tailwindcss from '@tailwindcss/vite';
 
 // })
 
-const baseFolder =
-    env.APPDATA !== undefined && env.APPDATA !== ''
-        ? `${env.APPDATA}/ASP.NET/https`
-        : `${env.HOME}/.aspnet/https`;
+// const baseFolder =
+//     env.APPDATA !== undefined && env.APPDATA !== ''
+//         ? `${env.APPDATA}/ASP.NET/https`
+//         : `${env.HOME}/.aspnet/https`;
 
-const certificateName = "gadgetmart-backend.client";
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+// const certificateName = "gadgetmart-backend.client";
+// const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+// const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
-if (!fs.existsSync(baseFolder)) {
-    fs.mkdirSync(baseFolder, { recursive: true });
-}
+// if (!fs.existsSync(baseFolder)) {
+//     fs.mkdirSync(baseFolder, { recursive: true });
+// }
 
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
-    }
-}
+// if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+//     if (0 !== child_process.spawnSync('dotnet', [
+//         'dev-certs',
+//         'https',
+//         '--export-path',
+//         certFilePath,
+//         '--format',
+//         'Pem',
+//         '--no-password',
+//     ], { stdio: 'inherit', }).status) {
+//         throw new Error("Could not create certificate.");
+//     }
+// }
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7030';
+    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://gadgetmart.runasp.net/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -67,10 +67,10 @@ export default defineConfig({
         //     }
         // },
         port: 5173, 
-        https: {
-            key: fs.readFileSync(keyFilePath),
-            cert: fs.readFileSync(certFilePath),
-        }
+        // https: {
+        //     key: fs.readFileSync(keyFilePath),
+        //     cert: fs.readFileSync(certFilePath),
+        // }
     },
     base: process.env.VITE_BASE_PATH || "/gadgetmart-frontend"
 })
